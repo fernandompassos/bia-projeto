6e237b61838ce82eac90a075bced0fc9f6e887c0
fernandoawsmp <fmpbh72@outlook.com>
2024-10-19 02:22:19 -0300

    Nova mudança botoes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "black" : "blue"}
-          text={showAdd ? "Fechar" : "Abrir"}
+          color={showAdd ? "yellow" : "green"}
+          text={showAdd ? "FBye bye" : "Incluir regiustro"}
           onClick={onAdd}
         />
       )}
