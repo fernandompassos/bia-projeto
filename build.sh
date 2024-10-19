b60036a33a3d230fb33c4cef9fa5a24d51f296af
fernandoawsmp <fmpbh72@outlook.com>
2024-10-19 02:16:29 -0300

    Mudei botoes

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
 aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin 767397833843.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia .
-docker tag bia:latest 767397833843.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
-docker push 767397833843.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+docker tag bia:latest 767397833843.dkr.ecr.us-east-1.amazonaws.com/bia2:$versao
+docker push 767397833843.dkr.ecr.us-east-1.amazonaws.com/bia2:$versao
 rm .env 2> /dev/null
 ./gerar-compose.sh
 rm bia-versao.zip
 zip -r bia-versao.zip docker-compose.yml
-git checkout docker-compose.yml
+#git checkout docker-compose.yml
