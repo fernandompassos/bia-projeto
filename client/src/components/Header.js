94730e58fa370ec775992ed0b415f99c39ca88dc
fernandoawsmp <fmpbh72@outlook.com>
2024-10-19 23:58:31 -0300

    Azul

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "orange"}
+          color={showAdd ? "red" : "blue"}
           text={showAdd ? "Bye bye" : "Insere"}
           onClick={onAdd}
         />
